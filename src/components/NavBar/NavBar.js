d6c43954fbf7e6f78ec63d0b393f0b889c197d9f
Javier Casas <javiercasas1986@gmail.com>
2022-07-24 22:43:23 -0300

    Desafio: crea tu landing-

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -5,17 +5,17 @@ import CartWidget from '../CartWidget'
 
 function NavBar() {
   return (
-    <Navbar bg="dark" expand="lg">
+    <Navbar className="navbar navbar-dark bg-dark"  expand="lg">
       <Container>
-        <Navbar.Brand className="text-white" href="#home">San Guchon</Navbar.Brand>
+        <Navbar.Brand href="#home">San Guchon</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link className="text-white" href="#home">Home</Nav.Link>
-            <Nav.Link className="text-white" href="#link">Los San Guchitos</Nav.Link>
-            <Nav.Link className="text-white" href="#link">Sobre Nosotros</Nav.Link>
-            <Nav.Link className="text-white" href="#link">Contacto</Nav.Link>
-            <Nav.Link className="text-white" href="#link"><CartWidget /></Nav.Link>
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Los San Guchitos</Nav.Link>
+            <Nav.Link href="#link">Sobre Nosotros</Nav.Link>
+            <Nav.Link href="#link">Contacto</Nav.Link>
+            <Nav.Link href="#link"><CartWidget /></Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
