62425337d435e65eb75b9e6715bfb436341c211b
Javier Casas <javiercasas1986@gmail.com>
2022-08-25 02:17:46 -0300

    Orders ready!

--- src/components/Cart/index.js
+++ src/components/Cart/index.js
@@ -1,10 +1,43 @@
 import React from "react";
 import { useCartContext } from "../CartContext";
 import CartItem from "../CartItem";
-
+import Button from "react-bootstrap/esm/Button";
+import { Link } from "react-router-dom";
+import { addDoc, collection, getFirestore,doc,updateDoc, increment } from "firebase/firestore";
 
 export default function Cart() {
   const { cartData, precioFinal,finalizarCompra, eliminarTodo } = useCartContext();
+
+  const order = {
+    buyer: {
+        name: 'Susana Horia',
+        email: 'susanitahoria@gmail.com',
+        phone: '3516321654',
+        address: 'Calle 13, piso 23'
+    },
+    items: cartData.map(producto => ({ id: producto.id, nombre: producto.nombre, precioUnitario: producto.precio, cantidad: producto.quantity, subtotal: producto.precio * producto.quantity })),
+    total: precioFinal(),
+}
+
+const generarCompra = () => {
+    const dataBase = getFirestore();
+    const ordersCollection = collection(dataBase, 'orders');
+    addDoc(ordersCollection, order)
+        .then(({ id }) => alert(`Sus plegarias han sido escuchadas. Su numero de compra es: ${id}`))
+        eliminarTodo()
+    cartData.forEach((producto)=> {
+        const itemRef = doc(dataBase, 'products', producto.id)
+        updateDoc(itemRef, {
+          stock: increment(-producto.quantity)
+        })
+    })
+} 
+
+
+
+
+
+
 
 
   return (
@@ -29,8 +62,13 @@ export default function Cart() {
       <div className="d-flex row justify-content-evenly">
         <h4 className="cartTotal">Monto total: ${precioFinal()}</h4>
         <div>
+        <div>
+        <Button as={Link} to="/" className="btn btn-info w-25">
+            Continuar comprando
+        </Button>
+        </div>
         <div> { eliminarTodo() } </div>
-        <div> {finalizarCompra()} </div>
+        <div onClick={generarCompra}> {finalizarCompra()} </div>
         </div>
       </div>
     </div>
