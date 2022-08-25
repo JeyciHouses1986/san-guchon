7f701c93d07be123327c8dc312e9beb5a7c56d59
Javier Casas <javiercasas1986@gmail.com>
2022-08-25 00:38:59 -0300

    Firestore

--- src/components/ItemListContainer/index.js
+++ src/components/ItemListContainer/index.js
@@ -2,61 +2,38 @@ import React, { useEffect, useState } from "react";
 import { useParams } from 'react-router-dom';
 import ItemList from '../ItemList';
 import './itemListContainer.css';
-import { collection, getDocs, query, where} from "firebase/firestore";
+import { collection, getDocs, query, where } from "firebase/firestore";
 import {DB} from "../Firebase/configs.js"
 
 
 export default function ItemListContainer () {
-    const [data, setData] = useState([]);
+  const [productsData, setProductsData] = useState([]);
     const {categoryId} = useParams()
 
     useEffect(()=>{
 
       const colRef = collection(DB, "Productos");
-    
+     
       if(categoryId){
         const  colFilterRef = query(colRef, 
-          where('cat', '==', categoryId))
+          where('category', '==', categoryId))
         getDocs(colRef)
-        .then(res=> setData(res.docs.map(prod => ({id:prod.id, ...prod.data()})))
+        .then(res=> setProductsData(res.docs.map(prod => ({id:prod.id, ...prod.data()})))
         )
         getDocs(colFilterRef)
-        .then(res=> setData(res.docs.map(prod => ({id:prod.id, ...prod.data()})))
+        .then(res=> setProductsData(res.docs.map(prod => ({id:prod.id, ...prod.data()})))
         )}else{
           getDocs(colRef)
-          .then(res=> setData(res.docs.map(prod => ({id:prod.id, ...prod.data()})))
+          .then(res=> setProductsData(res.docs.map(prod => ({id:prod.id, ...prod.data()})))
           )
         };  
       
     },[categoryId]);
 
 
-
-
-    /* const [productsData, setProductsData] = useState([]);
-    useEffect(() => {
-
-        setProductsData([]);
-
-        const productsDataPromise = getProductsByCategory (categoryId);
-
-        productsDataPromise.then(
-            (data) => {
-                setProductsData(data);
-            },
-            (err) => {
-                console.log(
-                    "Ha ocurrido un error al traer los productos: ",
-                    err
-                );
-            }
-        );
-    }, [categoryId]); */
-  
-
   return (
     <div>
-        {Array.isArray(data) && data.length === 0 ? (
+        {Array.isArray(productsData) && productsData.length === 0 ? (
                  <div className="waviy">
                  <span style={{'--i':'1'}}>Aguarde... </span>
                  <span style={{'--i':'2'}}>Los </span>
@@ -65,11 +42,9 @@ export default function ItemListContainer () {
                  <span style={{'--i':'5'}}>llegando </span>
                 </div>
             ) : (
-                <ItemList products={data} category={categoryId} />
+                <ItemList products={productsData} />
             )}
     </div>
   )
 }
-
-
 
